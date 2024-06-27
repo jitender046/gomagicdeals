50d40f0399831afb31ddac7408108391282295c6
jitender046 <itsinstagramhelp@gmail.com>
2024-06-27 18:57:27 +0530

    quickscreen test minor fixes

--- src/Quickscreen test.jsx
+++ src/Quickscreen test.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import './Quickscreen test.css';
 
 function QuizTest() {
@@ -8,24 +8,24 @@ function QuizTest() {
 
   const questions = [
     {
-      question: 'What is the capital of France?',
-      answers: ['Paris', 'London', 'Berlin', 'Rome'],
+      question: "What is the capital of France?",
+      answers: ["Paris", "London", "Berlin", "Rome"],
       correct: 0,
     },
     {
-      question: 'What is the largest planet in our solar system?',
-      answers: ['Earth', 'Saturn', 'Jupiter', 'Uranus'],
+      question: "What is the largest planet in our solar system?",
+      answers: ["Earth", "Saturn", "Jupiter", "Uranus"],
       correct: 2,
     },
     {
-      question: 'What is the smallest country in the world?',
-      answers: ['Vatican City', 'Monaco', 'Nauru', 'Tuvalu'],
+      question: "What is the smallest country in the world?",
+      answers: ["Vatican City", "Monaco", "Nauru", "Tuvalu"],
       correct: 0,
     },
     {
-      question: 'Thanks for giving the QuickscreenTest',
-      answers: [],
-      // correct: 0,
+      question: "Thanks for giving the QuickscreenTest",
+      answers: ["Vatican City", "Monaco", "Nauru", "Tuvalu"],
+      correct: 0,
     },
   ];
 
@@ -33,44 +33,69 @@ function QuizTest() {
     if (answerIndex === questions[currentQuestion].correct) {
       setScore(score + 1);
     }
-    setCurrentQuestion(currentQuestion + 1);
+    // setCurrentQuestion(currentQuestion + 1);
   };
 
   const handleShowResults = () => {
     setShowResults(true);
   };
 
+  const handleBack = () => {
+    if (currentQuestion > 0 && currentQuestion < questions.length - 1) {
+      setCurrentQuestion(currentQuestion - 1);
+    }
+  };
+
   return (
     <div className="screen">
-      <div className='QuickscreenTest'>
-      {showResults? (
-        <div>
-          <h2>Results</h2>
-          <p>
-            You scored {score} out of {questions.length-1}.
-          </p>
-        </div>
-      ) : (
-        <div className='Question-box'>
-          <h2>Quickscreen Test</h2>
-          {/* <p>Question {currentQuestion + 1} of {questions.length-1}</p> */}
-          <h3 className='Question'>{questions[currentQuestion].question}</h3>
-          <ul>
-            {questions[currentQuestion].answers.map((answer, index) => (
-              <li key={index}>
-                <button onClick={() => handleAnswer(index)}>{answer}</button>
-              </li>
-            ))}
-          </ul>
-          {/* button change code here... */}
-          {currentQuestion === questions.length -1? (
-            <h2 id='center'><button  onClick={handleShowResults}>Show Results</button></h2>
-          ) : (
-            <h2 id='center'><button  onClick={() => setCurrentQuestion(currentQuestion + 1)}> Next Question &raquo;</button></h2>
-          )}
-        </div>
-      )}
-    </div>
+      <div className="QuickscreenTest">
+        {showResults ? (
+          <div className="view_result">
+            <h2>Results</h2>
+            <p>
+              You scored {score} out of {questions.length}.
+            </p>
+          </div>
+        ) : (
+          <div className="Question-box">
+            <h2>Quickscreen Test</h2>
+            {currentQuestion === questions.length ? (
+              <h3 id="center">Thanks for taking the QuickscreenTest!</h3>
+            ) : (
+              <h3 className="Question">{questions[currentQuestion].question}</h3>
+            )}
+            {currentQuestion === questions.length  ? (
+              <p id="center">If you want to see your result then click on Show Result button.</p>
+            ) : (
+              <ul key={currentQuestion}>
+                {questions[currentQuestion].answers.map((answer, index) => (
+                  <li key={index}>
+                    <label>
+                      <input
+                        type="radio"
+                        id={`check-${index}`}
+                        name="answer"
+                        onClick={() => handleAnswer(index)}
+                      />
+                      {answer}
+                    </label>
+                  </li>
+                ))}
+              </ul>
+            )}
+          
+            {currentQuestion === questions.length  ? (
+              <h2 id="center"><button onClick={handleShowResults}>Show Results</button></h2>
+            ) : (
+              <h2 id="center">
+                <button onClick={handleBack}>&laquo; Back Question</button>
+                <button onClick={() => setCurrentQuestion(currentQuestion + 1)}> Next Question &raquo;</button>
+              </h2>
+
+            )}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
