983eb79a8290224743ea2ceaf0e1d366c159dc4d
jitender046 <itsinstagramhelp@gmail.com>
2024-06-27 19:07:59 +0530

    remove extra code

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Header  from './Header';
 // import Footer from "./Footer";
 // import Weather from './WeatherApp';
 import QuizTest from './Quickscreen test';
-import Question from './QuizTest';
 
 function App() {
   return (
