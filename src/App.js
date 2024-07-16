1a0acf3eb140d88494faf54a57ba4dc8572d2189
jitender046 <itsinstagramhelp@gmail.com>
2024-07-16 10:44:35 +0530

    footer css fix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-import Header  from './Header';
+// import Header  from './Header';
 // import TodoApp from './TodoApp';
 // import Myskills from './Myskills'
 // import Footer from "./Footer";
 // import Weather from './WeatherApp';
 // import QuizTest from './Quickscreen test';
-import Youtube from './YoutubeApp';
+// import Youtube from './YoutubeApp';
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Youtube/>
+      {/* <Header/> */}
+      {/* <Youtube/> */}
     </div>
   );
 }
