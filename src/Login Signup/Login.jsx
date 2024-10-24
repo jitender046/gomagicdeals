595b842532d784ce730f9bec5e473c487516033e
jitender046 <itsinstagramhelp@gmail.com>
2024-10-24 14:59:22 +0530

    Sign in page ui change

--- src/Login Signup/Login.jsx
+++ src/Login Signup/Login.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import { Button, TextField } from "@mui/material";
+import { TextField } from "@mui/material";
 import swal from "sweetalert";
 import "./Loginsignup.css";
-import Header from '../Header Footer/Header.js';
-import Footer from '../Header Footer/Footer.jsx';
+import appIcon from "../Images/icons/appIcon.svg";
+import Header from "../Header Footer/Header.js";
+import Footer from "../Header Footer/Footer.jsx";
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -35,60 +36,86 @@ function Login() {
 
   return (
     <>
-    <Header />
-    <div className="App">
-      <div className="all-content-center">
-        <div className="login">
-          <div className="form">
-            <p>Welcome to</p>
-            <h1> CoffeeBreak ☕</h1>
-            <p>
-              We serve coffee from all around the world.
+      <Header />
+      <div className="App">
+        <div className="all-content-center">
+          <div className="login">
+            <div className="form">
+              <img src={appIcon} alt="App Logo" className="app-logo" />{" "}
+              <h1>Sign In</h1>
+              <p class="text-warning">Access to your account</p>
               <br />
-              Login your account and enjoy it!
-            </p>
-            <br />
-            <br />
-            {/* Email id:  */}
-            <TextField
-              id="standard-basic"
-              label="Enter  Email"
-              InputLabelProps={{ style: { color: "white" } }}
-              variant="standard"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <br />
-            <br />
-            <TextField
-              id="standard-password-input"
-              label="Password"
-              InputLabelProps={{ style: { color: "white" } }}
-              autoComplete="current-password"
-              variant="standard"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <br />
-            <br />
-            <Button variant="contained" onClick={handleLogin}>
-              Login
-            </Button>
-            <br />
-            <br />
-            <br />
-            <br />
-            <p className="already-account"> If don't have an account ?</p>
-            <Button
-              variant="contained"
-              className="already-account-button"
-              color="secondary"
-            >
-              Register
-            </Button>
+              <br />
+              <TextField
+                className="input-filed"
+                id="standard-basic"
+                label="Email or Phone"
+                InputLabelProps={{ style: { color: "black" } }}
+                variant="standard"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <br />
+              <br />
+              <TextField
+                className="input-filed"
+                id="standard-password-input"
+                label="Password"
+                InputLabelProps={{ style: { color: "black" } }}
+                autoComplete="current-password"
+                variant="standard"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <br />
+              <div className="rememberMe">
+                <input
+                  type="checkbox"
+                  name=""
+                  id="Checkbox"
+                  className="checkbox"
+                />
+                <label for="">Remember Me</label>
+              </div>
+              <br />
+              <button
+                className="signin-btn"
+                onClick={handleLogin}
+                id="Submit-btn"
+                disabled
+              >
+                Sign In
+              </button>
+              <br />
+              <br />
+              <a href="/" className="forget-password">
+                Forgot Password?
+              </a>
+              <br />
+              <div class="signInWith">
+                <span></span>
+                <p>or sign in with</p>
+                <span></span>
+              </div>
+              <div class="signIn-tools">
+                <img src={require("../Images/icons/google.png")} alt="google" />
+                <img
+                  src={require("../Images/icons/twitter.png")}
+                  alt="twittter"
+                />
+                <img
+                  src={require("../Images/icons/facebook.png")}
+                  alt="facebook"
+                />
+              </div>
+              <br />
+              <p className="already-account">
+                {" "}
+                Don't have an account ? <a href="./sign-up.html">Sign Up</a>
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <Footer />
+      <Footer />
     </>
   );
 }
